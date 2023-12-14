5e9e3d43fea26e63081a122f67a75ef3bd3bf450
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-14 10:55:57 +0530

    Add files via upload

---
+++ ClerkManagementApp/DAL/Services/ClerkRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using ClerkManagementApp.DAL.Interfaces;
+
+namespace ClerkManagementApp.DAL.Services
+{
+    public class ClerkRepository : Interfaces.IClerkRepository
+    {
+        private ClerkDbContext _context;
+
+        public ClerkRepository(ClerkDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.ClerkModel GetById(string id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
